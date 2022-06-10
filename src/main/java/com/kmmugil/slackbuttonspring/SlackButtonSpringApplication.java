128f8e9a36fe2db16f196e4b46884488ee059e13
kmmugil <kmm.mugil@gmail.com>
2022-06-10 11:54:57 +0530

    Project initial commit

---
+++ src/main/java/com/kmmugil/slackbuttonspring/SlackButtonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kmmugil.slackbuttonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackButtonSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackButtonSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmmugil/slackbuttonspring/SlackButtonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmmugil.slackbuttonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackButtonSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
