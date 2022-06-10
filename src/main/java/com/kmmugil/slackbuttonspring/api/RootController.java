94a429ed9ce07ff11630414e005aee189ed697d9
kmmugil <kmm.mugil@gmail.com>
2022-06-10 20:42:54 +0530

    Reverted spring boot parent version

--- src/main/java/com/kmmugil/slackbuttonspring/api/RootController.java
+++ src/main/java/com/kmmugil/slackbuttonspring/api/RootController.java
@@ -2,7 +2,7 @@ package com.kmmugil.slackbuttonspring.api;
 
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
