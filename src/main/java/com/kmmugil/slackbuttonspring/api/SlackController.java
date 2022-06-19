d083f94f6dbc27f194da0011442d471f22150a7f
kmmugil <kmm.mugil@gmail.com>
2022-06-19 20:35:37 +0530

    Event receiver API for responding challenge from slack

--- src/main/java/com/kmmugil/slackbuttonspring/api/SlackController.java
+++ src/main/java/com/kmmugil/slackbuttonspring/api/SlackController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "SlackController")
 @RequestMapping(value = "/api/slack")
@@ -57,6 +54,12 @@ public class SlackController {
             logger.error(e.getMessage(), e);
             return ResponseEntity.internalServerError().body(null);
         }
+    }
+
+    @PostMapping("/event")
+    public ResponseEntity<?> slackEventReceiver(@RequestParam(name = "challenge", required = false) String challenge) {
+        logger.info("Event Received from Slack ...");
+        return ResponseEntity.ok(challenge);
     }
 
 }
